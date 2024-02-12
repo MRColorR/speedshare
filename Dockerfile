12d5103f8feed339528adc1f763dc6af99643ea4
MRColor <mattiar11@gmail.com>
2024-02-12 12:39:24 +0100

    CHG - new Dockerfile to install during build new Docker Build and Push workflow.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,30 @@
 FROM alpine
+
 # Update and Upgrade automatically without prompt
 RUN apk update && apk upgrade
 RUN apk add --no-cache ca-certificates dbus wget
+
 # Delete cache and temp files to reduce image size
 RUN rm -rf /var/cache/apk/* /tmp/*
+
+# Define the download URL base
+ARG DOWNLOAD_URL=https://api.speedshare.app/download/linux/cli
+
+# Determine the architecture and download the correct SpeedShareCLI binary
+ARG ARCH
+RUN if [ "$(uname -m)" = "x86_64" ]; then \
+        ARCH="amd64"; \
+    elif [ "$(uname -m)" = "aarch64" ]; then \
+        ARCH="arm64"; \
+    else \
+        echo "Unsupported architecture: $(uname -m)" && exit 1; \
+    fi && \
+    echo "Downloading SpeedShareCLI for ${ARCH}" && \
+    wget -q "${DOWNLOAD_URL}/${ARCH}" -O /usr/local/bin/SpeedShareCLI && \
+    chmod +x /usr/local/bin/SpeedShareCLI
+
+# Add the entrypoint script
 ADD entrypoint.sh /root/
 RUN chmod +x /root/entrypoint.sh
+
 ENTRYPOINT [ "/root/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,15 @@
 #!/bin/sh
-# Get system arch (amd64 or arm64)
-ARCH=$(uname -m)
-if [ "$ARCH" = "x86_64" ]; then
-    ARCH="amd64"
-elif [ "$ARCH" = "aarch64" ]; then
-    ARCH="arm64"
-else
-    echo "Unsupported architecture: $ARCH"
-    exit 1
-fi
 
 # Check if a SPEEDSHARE_UUID is provided as an environment variable, else gives a warning message and a new UUID is generated
 if [ -z "$SPEEDSHARE_UUID" ]; then
-    echo "Warning: No UUID provided. A new random UUID will be generated"
+    echo "Warning: No UUID provided. A new random UUID will be generated."
     echo "This will cause the device to be seen as a new device in the SpeedShare app every time it is restarted."
     echo "To avoid this, please provide a UUID as an environment variable."
     dbus-uuidgen > /etc/machine-id
 else
     echo "$SPEEDSHARE_UUID" > /etc/machine-id
-    echo "UUID provided: $SPEEDSHARE_UUID"
+    echo "Using provided UUID: $SPEEDSHARE_UUID"
 fi
 
-# Download, install and run the program
-wget -q https://api.speedshare.app/download/linux/cli/$ARCH -O /root/SpeedShareCLI
-chmod +x /root/SpeedShareCLI
-/root/SpeedShareCLI connect -p $CODE 
+# Run the program
+SpeedShareCLI connect -p $CODE
